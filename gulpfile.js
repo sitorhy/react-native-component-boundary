74047f46f2acd60b20fa3b118f3a6bd6fdd8f1e3
sitorhy <sitorhy@gmail.com>
2023-09-13 02:27:24 +0800

    refactor project

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,58 @@ const path = require("path");
 const ts = require('gulp-typescript');
 const babel = require("gulp-babel");
 
-const LIB_HOME = path.join("app/lib");
+const LIB_HOME = path.join("lib");
+
+function copyFile(location, targetSubDir) {
+    const dir = path.dirname(location);
+    const dest = [
+        `web/${targetSubDir}/`.replace(/\\/g, "/"),
+        `app/${targetSubDir}/`.replace(/\\/g, "/")
+    ];
+    const index = dir.indexOf(targetSubDir);
+    if (index >= 0) {
+        const iDir = index + targetSubDir.length;
+        const sub = dir.substring(iDir + 1);
+        dest.forEach(i => {
+            gulp.src(location, {
+                base: dir
+            }).pipe(gulp.dest(i + sub));
+        });
+    }
+}
+
+function deleteFile(location, targetSubDir) {
+    const dest = [
+        `app/${targetSubDir}/`.replace(/\\/g, "/"),
+        `web/${targetSubDir}/`.replace(/\\/g, "/")
+    ];
+    dest.forEach(i => {
+        fs.unlink(i + location, function () {
+            console.log(`unlink "${location}"`);
+        });
+    });
+}
+
+gulp.task("deploy", async () => {
+    gulp.src(LIB_HOME + "/**/*.ts", {
+        base: LIB_HOME
+    }).pipe(gulp.dest(`app/lib`)).pipe(gulp.dest(`web/lib`));
+});
+
+gulp.task("dev", gulp.series(["deploy"], async () => {
+    const libWatcher = gulp.watch(LIB_HOME);
+    libWatcher.on("change", function (location, stats) {
+        copyFile(location, "lib");
+    });
+
+    libWatcher.on("add", function (location, stats) {
+        copyFile(location, "lib");
+    });
+
+    libWatcher.on("unlink", function (location, stats) {
+        deleteFile(location, "lib");
+    });
+}));
 
 gulp.task("build", async () => {
     if (!fs.existsSync("dist")) {
